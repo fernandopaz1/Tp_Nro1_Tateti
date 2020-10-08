e8b6e9fb585124a546e6f201c2c36d60a28bb380
Nacho2001 <nachosotelo9@gmail.com>
2020-10-08 13:21:42 -0300

    Ahora los jgadores pueden poner una foto para jugar y tambien cambie la visualizacion de la ventana, ya no es una venta default si no que se adapta a la interfaz de ventanas del SO que tengamos

--- src/visual/Ventana.java
+++ src/visual/Ventana.java
@@ -8,6 +8,15 @@ import javax.swing.JPanel;
 
 
 import logica.Juego;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 
 
 public class Ventana {
@@ -18,6 +27,11 @@ public class Ventana {
 	private VentanaFinal ventanaFinal;
 	private VentanaDeJuego ventanaDeJuego;
 	private JPanel panelDeJuego;
+	private JLabel Foto1;
+	private JLabel Foto2;
+	private JButton Boton1;
+	private JButton Boton2;
+	private final Action action = new SwingAction();
 
 	/**
 	 * Launch the application.
@@ -53,6 +67,7 @@ public class Ventana {
 		frame.getContentPane().setLayout(null);
 		
 		panelDeJuego = new JPanel();
+		panelDeJuego.setBounds(0, 0, 0, 0);
 		frame.getContentPane().add(panelDeJuego);
 		
 
@@ -70,6 +85,34 @@ public class Ventana {
 		ventanaInicial=new VentanaInicial(panelInicial, panelDeJuego, juego);
 		
 		ventanaFinal = new VentanaFinal(panelFinal, panelDeJuego, panelInicial, ventanaDeJuego);
+		panelInicial.setLayout(null);
+		
+//		Foto1 = new JLabel("Foto jugador 1");
+//		Foto1.setBounds(361, 44, 63, 48);
+//		panelInicial.add(Foto1);
+//		
+//		Foto2 = new JLabel("Foto jugador 2 ");
+//		Foto2.setBounds(361, 137, 63, 48);
+//		panelInicial.add(Foto2);
+//		
+//		Boton1 = new JButton("New button");
+//		Boton1.setAction(action);
+//		Boton1.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				JFileChooser fc = new JFileChooser();
+//				fc.setDialogTitle("Buscar foto");
+//				
+//				if (fc.showOpenDialog() == JFileChooser.APPROVE_OPTION) {
+//					File archivo = new File(fc.getSelectedFile().toString());
+//				}
+//			}
+//		});
+//		Boton1.setBounds(335, 103, 89, 23);
+//		panelInicial.add(Boton1);
+//		
+//		Boton2 = new JButton("New button");
+//		Boton2.setBounds(335, 196, 89, 23);
+//		panelInicial.add(Boton2);
 		
 		
 		ventanaDeJuego= new VentanaDeJuego(panelDeJuego,juego,ventanaFinal);
@@ -88,4 +131,12 @@ public class Ventana {
 
 	}
 	
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
 }
--- src/visual/VentanaInicial.java
+++ src/visual/VentanaInicial.java
@@ -1,11 +1,20 @@
 package visual;
 
+import java.awt.Color;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.LineBorder;
 
 import logica.Juego;
 
@@ -17,6 +26,10 @@ public class VentanaInicial {
 	private JPanel panelDeJuego;
 	private Juego newGame;
 	private JLabel msjError;
+	private JLabel Foto1;
+	private JLabel Foto2;
+	private JButton Boton1;
+	private JButton Boton2;
 	
 
 	public VentanaInicial(JPanel panelInicial, JPanel panelDeJuego, Juego newGame) {
@@ -32,7 +45,7 @@ public class VentanaInicial {
 		javax.swing.JLabel msjError = new javax.swing.JLabel(textoDeError);
 		msjError.setForeground(new java.awt.Color(105, 105, 105));
 		msjError.setFont(new java.awt.Font("Sitka Banner", java.awt.Font.PLAIN, 16));
-		msjError.setBounds(10, 220, 450, 27);
+		msjError.setBounds(10, 238, 450, 27);
 		msjError.setVisible(false);
 		panelInicial.add(msjError);
 		return msjError;
@@ -47,36 +60,105 @@ public class VentanaInicial {
 			panelInicial.setBounds(0, 0, 434, 261);
 			panelInicial.setLayout(null);
 			
+			
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());     //Esto lo q hace es darle una vista de tipo windows 
+			} catch (ClassNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (InstantiationException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IllegalAccessException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (UnsupportedLookAndFeelException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			Foto1 = new JLabel("");
+			Foto1.setBounds(250, 66, 63, 48);
+			Foto1.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+			panelInicial.add(Foto1);
+			
+			Foto2 = new JLabel("");
+			Foto2.setBounds(250, 137, 63, 48);
+			Foto2.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+			panelInicial.add(Foto2);
+			
+			Boton1 = new JButton("Elegir foto");
+			Boton1.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JFileChooser fc = new JFileChooser();
+					fc.setDialogTitle("Buscar foto");
+		
+						if (fc.showOpenDialog(panelInicial) == JFileChooser.APPROVE_OPTION) {
+								
+								
+								Image img= new ImageIcon(fc.getSelectedFile().toString()).getImage();
+								ImageIcon img2=new ImageIcon(img.getScaledInstance(78, 124, Image.SCALE_SMOOTH));
+
+								Foto1.setIcon(img2); 
+								
+							}
+						}
+					
+				
+			});
+			Boton1.setBounds(335, 79, 89, 23);
+			panelInicial.add(Boton1);
+			
+			Boton2 = new JButton("Elegir foto");
+			Boton2.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JFileChooser fc = new JFileChooser();
+					fc.setDialogTitle("Buscar foto");
+		
+						if (fc.showOpenDialog(panelInicial) == JFileChooser.APPROVE_OPTION) {
+								
+								
+								Image img= new ImageIcon(fc.getSelectedFile().toString()).getImage();
+								ImageIcon img2=new ImageIcon(img.getScaledInstance(78, 124, Image.SCALE_SMOOTH));
+
+								Foto2.setIcon(img2); 
+								
+							}
+					
+				}
+			});
+			Boton2.setBounds(335, 150, 89, 23);
+			panelInicial.add(Boton2);
+			
 			javax.swing.JLabel titulo = new javax.swing.JLabel("TA - TE - TORO");
 			titulo.setVerticalAlignment(SwingConstants.BOTTOM);
 			titulo.setHorizontalAlignment(SwingConstants.CENTER);
 			titulo.setForeground(new java.awt.Color(255, 69, 0));
 			titulo.setFont(new java.awt.Font("Showcard Gothic", java.awt.Font.PLAIN, 38));
-			titulo.setBounds(73, 22, 286, 48);
+			titulo.setBounds(73, 5, 286, 48);
 			panelInicial.add(titulo);
 			
 			javax.swing.JLabel msj_jugador1 = new javax.swing.JLabel("Jugador 1: ");
 			msj_jugador1.setForeground(new java.awt.Color(105, 105, 105));
 			msj_jugador1.setFont(new java.awt.Font("Sitka Banner", java.awt.Font.PLAIN, 16));
-			msj_jugador1.setBounds(82, 81, 91, 27);
+			msj_jugador1.setBounds(60, 81, 91, 27);
 			panelInicial.add(msj_jugador1);
 			
 			javax.swing.JLabel msj_jugador2 = new javax.swing.JLabel("Jugador 2: ");
 			msj_jugador2.setForeground(new java.awt.Color(105, 105, 105));
 			msj_jugador2.setFont(new java.awt.Font("Sitka Banner", java.awt.Font.PLAIN, 16));
-			msj_jugador2.setBounds(80, 119, 91, 27);
+			msj_jugador2.setBounds(60, 150, 91, 27);
 			panelInicial.add(msj_jugador2);
 			
 			textJugador1 = new javax.swing.JTextField();
 			textJugador1.setBackground(new java.awt.Color(240, 230, 140));
-			textJugador1.setBounds(190, 81, 86, 20);
+			textJugador1.setBounds(140, 81, 86, 20);
 			panelInicial.add(textJugador1);
 			textJugador1.setColumns(10);
 			
 			textJugador2 = new javax.swing.JTextField();
 			textJugador2.setBackground(new java.awt.Color(240, 230, 140));
 			textJugador2.setColumns(10);
-			textJugador2.setBounds(190, 121, 86, 20);
+			textJugador2.setBounds(140, 150, 86, 20);
 			panelInicial.add(textJugador2);
 			
 			
@@ -85,7 +167,7 @@ public class VentanaInicial {
 			boton_jugar.setForeground(new java.awt.Color(255, 99, 71));
 			boton_jugar.setFont(new java.awt.Font("Showcard Gothic", java.awt.Font.PLAIN, 16));
 			boton_jugar.setBackground(new java.awt.Color(240, 230, 140));
-			boton_jugar.setBounds(158, 169, 118, 48);
+			boton_jugar.setBounds(158, 190, 118, 48);
 			panelInicial.add(boton_jugar);
 			
 			msjError = agregarMsjError("");
